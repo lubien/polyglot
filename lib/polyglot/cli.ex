f924d30a8abbfec439cebe9be4a2515244900e54
lubien <lubien1996@gmail.com>
2016-11-10 08:29:16 -0300

    Update help

--- lib/polyglot/cli.ex
+++ lib/polyglot/cli.ex
@@ -27,6 +27,14 @@ defmodule Polyglot.CLI do
   def process({[help: true], _, _}) do
     IO.puts """
     Usage: polyglot username [--help]
+
+      -t, --token   GitHub personal access token.
+
+                    GitHub limits to 60 requests in a small
+                    amount of time. Use a token to have a
+                    limit of 1000 requests.
+
+      --forks       Count forks too
     """
   end
 
